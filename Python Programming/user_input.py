e78b6ef7c301fc3f99beab0e4896c46bfb6059b1
Ngoyarez <brianngoya764@yahoo.com>
2024-04-03 08:41:01 +0300

    User Input

---
+++ Python Programming/user_input.py
@@ -0,0 +1,6 @@
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+location = input("Where do you live? ")
+
+message = f"Hello {name}, you are {age} years old and live in {location}."
+print(message)
